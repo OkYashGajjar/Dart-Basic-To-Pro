2fbab1fe041afd868dec689c38cb4e6113de6dd4
Yash Gajjar <gajjaryash9999@gmail.com>
2024-12-22 21:49:26 +0530

    Added Common Syntaxes

---
+++ SyntaxOfVariables.dart
@@ -0,0 +1,21 @@
+void main() {
+  int num1 = 10; //Stores an integer value
+
+  double num2 = 20.10; //Stores larger integer value
+
+  num num4 = 26.90; //Stores an integer and fraction both value
+  num num5 = -60;
+
+  bool say = true; //Only stores true or false values
+  bool againSay = false;
+
+  var stores = 'Hello'; // var can store string,int,double all.
+  var stores2 = 10;
+  var stores3 = 10.2;
+  var stores5 = null;
+
+  String? stores4 = 'Welcome'; // Nullable String
+
+  // \n stands for next line.
+  print(' it can store everything like this $num1\n $num2\n $num4 $num5 $say $againSay $stores $stores2 $stores3 $stores5 $stores4');
+}
---
+++ SyntaxToPrint.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+
+  // Method 1 :
+  print('Hello world 1.');
+
+  // Method 2 :
+  print("Hello world 2");
+
+  // Method 3 :
+  stdout.write('Hello world 3'); // this requires to import 'dart:io';
+
+}
